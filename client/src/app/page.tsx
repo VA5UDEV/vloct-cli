c686b3821f4fafc28c7077a20c397bf1d403075a
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-04-08 04:51:53 +0530

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function page() {
+  return (
+    <div className="center h-screen">
+      <Button>hiiirrr</Button>
+      gdjhddrrii
+    </div>
+  );
+}
+
+export default page;
